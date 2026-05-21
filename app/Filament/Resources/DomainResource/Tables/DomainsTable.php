695fe98db2dab103232d54abe1e53bdcdd5d64b6
Malebestia <malebestia@yahoo.it>
2026-05-21 09:53:57 +0200

    .

--- app/Filament/Resources/DomainResource/Tables/DomainsTable.php
+++ app/Filament/Resources/DomainResource/Tables/DomainsTable.php
@@ -13,7 +13,7 @@ class DomainsTable extends XotBaseResourceTable
     /**
      * @return array<string, Column>
      */
-    public function getTableColumns(): array
+    public static function getTableColumns(): array
     {
     /**
      * @return array<int\|string, \Filament\Tables\Columns\Column>
