2533867e6297cc8d2e4ce491d20b347f5c22ebcd
marco76tv <marco.sottana@gmail.com>
2026-05-26 21:09:46 +0200

    .

--- app/Filament/Resources/DomainResource/Pages/EditDomain.php
+++ app/Filament/Resources/DomainResource/Pages/EditDomain.php
@@ -9,5 +9,5 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 class EditDomain extends XotBaseEditRecord
 {
-    protected static string $resource = DomainResource::class;
+    public static string $resource = DomainResource::class;
 }
