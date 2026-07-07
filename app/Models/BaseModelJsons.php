cc86e30969721e2b31aea5d7510e2d2a58afb398
marco76tv <marco.sottana@gmail.com>
2026-07-07 09:59:44 +0200

    .

--- app/Models/BaseModelJsons.php
+++ app/Models/BaseModelJsons.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Models;
 
+use Modules\Tenant\Contracts\SushiToJsonsContract;
 use Modules\Tenant\Models\Traits\SushiToJsons;
 
 /**
@@ -12,7 +13,7 @@ use Modules\Tenant\Models\Traits\SushiToJsons;
  * @property array<string, mixed> $form
  * @property array<string, mixed> $schema
  */
-abstract class BaseModelJsons extends BaseModel
+abstract class BaseModelJsons extends BaseModel implements SushiToJsonsContract
 {
     use SushiToJsons;
 }
