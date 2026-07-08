9cc9efe79616bb6b6baff758407c96fbcfacd604
marco76tv <marco.sottana@gmail.com>
2026-07-08 09:23:01 +0200

    .

--- database/factories/TenantSubscriptionFactory.php
+++ database/factories/TenantSubscriptionFactory.php
@@ -19,6 +19,9 @@ class TenantSubscriptionFactory extends Factory
     /**
      * Define the model's default state.
      */
+    /**
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
