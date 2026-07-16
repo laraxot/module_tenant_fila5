c4b94856f6e0fcb01a70ce824c7448b40bd83ace
marco76tv <marco.sottana@gmail.com>
2026-07-16 09:48:51 +0200

    .

--- app/Models/TenantDomain.php
+++ app/Models/TenantDomain.php
@@ -20,6 +20,7 @@ use Sushi\Sushi;
  * @property string|null $status
  * @property string|null $verification_token
  * @property Carbon|null $verified_at
+ *
  * @method static Builder|TenantDomain newModelQuery()
  * @method static Builder|TenantDomain newQuery()
  * @method static Builder|TenantDomain query()
@@ -30,10 +31,13 @@ use Sushi\Sushi;
  * @method static Builder|TenantDomain whereStatus($value)
  * @method static Builder|TenantDomain whereVerificationToken($value)
  * @method static Builder|TenantDomain whereVerifiedAt($value)
+ *
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
  * @property ProfileContract|null $deleter
+ *
  * @method static TenantDomainFactory factory($count = null, $state = [])
+ *
  * @mixin \Eloquent
  */
 class TenantDomain extends BaseModel
--- tests/Fixtures/Traits/TenantPhpstanProbeModel.php
+++ tests/Fixtures/Traits/TenantPhpstanProbeModel.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Modules\Tenant\Tests\Fixtures\Traits;
 
+use Illuminate\Support\Carbon;
 use Modules\Tenant\Models\BaseModel;
 
 /**
- * @property int|string|null                 $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property int|null                        $created_by
- * @property int|null                        $updated_by
+ * @property int|string|null $id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property int|null $created_by
+ * @property int|null $updated_by
  */
 abstract class TenantPhpstanProbeModel extends BaseModel
 {
