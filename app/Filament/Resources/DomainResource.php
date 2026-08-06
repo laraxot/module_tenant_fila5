6ea10df244b511d4abf1d25009ecb185262c937b
Marco Xot <marco76tv@hotmail.com>
2026-08-06 19:29:33 +0200

    .

--- app/Filament/Resources/DomainResource.php
+++ app/Filament/Resources/DomainResource.php
@@ -17,8 +17,12 @@ class DomainResource extends XotBaseResource
 {
     protected static ?string $model = Domain::class;
 
-    #[Override]
-    public static function getFormSchema(): array
+    /**
+     * Schema legacy del form: la sorgente di verità è DomainForm::getFormSchema().
+     *
+     * @return array<string, \Filament\Schemas\Components\Component>
+     */
+    public static function getFormSchemaOld(): array
     {
         return [
             'title' => TextInput::make('title')
