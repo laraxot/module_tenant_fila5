7073aca65755ae30424efb8854f6ae2fd4f7f880
Marco Xot <marco76tv@hotmail.com>
2026-09-06 16:05:40 +0200

    chore(Tenant): sync all changes

--- app/Models/TestSushiModel.php
+++ app/Models/TestSushiModel.php
@@ -43,7 +43,6 @@ use Modules\Xot\Models\Traits\HasXotFactory;
  */
 class TestSushiModel extends BaseModel
 {
-    /** @use HasXotFactory<TestSushiModelFactory> */
     use HasXotFactory;
 
     use SushiToJson;
